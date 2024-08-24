5b6a5c24baeb852fcc1e856403bef80684c362c8
Vinicius Lisboa <lisboamvinicius@gmail.com>
2024-08-24 15:22:43 +0000

    alterando a frase do desafio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
 function Home() {
-    return <h1>Sextou</h1>
+    return (
+        <div style={{
+            'width': '80%',
+            'margin': 'auto',
+            'fontFamily': 'Cambria',
+        }}>
+            <h1>Liesley Tavares de Oliveira</h1>
+            <p>Você é o amor da minha vida e meu futuro marido. Eu te amo como nunca amei outro alguém. Você tem meu coração e eu nunca poderei te esquecer.</p>
+
+            <blockquote style={{ 
+                'fontSize': '15px',
+                'margin': 'auto',
+                'text-align': 'center'            
+            }}>
+                <p><i>Talvez eu goste ainda mais de nós<br />
+                Num dia inteiro colados, a sós<br />
+                Com tempo pra se discorrer a dois<br />
+                Sentir sua pele sem tua roupa</i></p>
+
+                <p><i>Enquanto você dorme, eu cheiro você<br />
+                Pra guardar na memória o tom<br />
+                Do meu veludo cor marrom, ai<br />
+                Eu não tô a fim de desgrudar<br />
+                Mas quero fazer, dos dias, a paz<br />
+                Para fazer um escarcéu com teu sorriso<br />
+                Nem ligo, a gente pode demorar</i></p>
+
+                <p><i><strong>Liniker, Veludo Marrom </strong></i>🤎</p>
+            </blockquote>
+
+            <p>Se gostou da surpresa, me manda uma emoji de coração marrom no Whatsapp. De novo, te amo muito!</p>
+
+            <p style={{ 'text-align': 'right' }}>Com amor, o sempre seu, <br /><strong>Vini</strong>.</p>
+        </div>
+    )
 }
 
 export default Home;
